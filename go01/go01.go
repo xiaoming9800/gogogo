32862385e36b49dbeb45838dea70c3ea01bd9441
xiaoming9800 <41463184+xiaoming9800@users.noreply.github.com>
2019-02-03 14:40:58 +0800

    九九乘法表

---
+++ go01/go01.go
@@ -0,0 +1,21 @@
+package go01
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func JiujiuBiao(w http.ResponseWriter, r *http.Request) {
+	jiujiu(1, w)
+}
+
+func jiujiu(n int, w http.ResponseWriter) {
+	for i := 1; i <= n; i++ {
+		fmt.Fprint(w, i, "*", n, "=", i*n, "\t")
+	}
+	if n == 9 {
+		return
+	}
+	fmt.Fprintln(w)
+	jiujiu(n+1, w)
+}
