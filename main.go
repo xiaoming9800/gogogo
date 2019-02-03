165ae478875a74691c477521933f390ef359c938
xiaoming9800 <41463184+xiaoming9800@users.noreply.github.com>
2019-02-03 14:21:43 +0800

    Hello World WEB

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/sayHi", SayHi)
+	er := http.ListenAndServe(":8080", nil)
+	if er != nil {
+		log.Fatal("ListenAndServe", er)
+	}
+}
+
+//localhost:8080
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
+//localhost:8080/sayHi?name=xiaoming
+func SayHi(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	fmt.Fprint(w, "Hi!", name)
 }
